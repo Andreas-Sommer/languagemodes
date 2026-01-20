628154c8d3ee09733f74984c042bdcf5abb55b0d
Andreas Sommer <sommer@belsignum.com>
2026-01-20 15:03:00 +0100

    Remove unused `Site` import in `SiteConfiguration`

--- Classes/Xclass/SiteConfiguration.php
+++ Classes/Xclass/SiteConfiguration.php
@@ -2,8 +2,6 @@
 
 namespace Belsignum\Languagemodes\Xclass;
 
-use TYPO3\CMS\Core\Site\Entity\Site;
-
 class SiteConfiguration extends \TYPO3\CMS\Core\Configuration\SiteConfiguration
 {
     protected bool $individualLanguageModeProcessed = false;
